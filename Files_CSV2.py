03af8f61bff76b9dafb2396ce8a555f4f8e2c5ff
Vamsi Kamal <vamsikamal.net@gmail.com>
2020-10-09 09:00:54 +0530

    List Example

--- Files_CSV2.py
+++ Files_CSV2.py
@@ -2,5 +2,9 @@ import csv
 f = open('emp.csv', 'r')
 r = csv.reader(f)
 data = list(r)
-for line in data:
-    print(line)
+print('Employee details: ', end='\n\n')
+
+for line in data[1:]:
+    for i in range(len(data[0])):
+        print(data[0][i],' : ',line[i])
+    print()
